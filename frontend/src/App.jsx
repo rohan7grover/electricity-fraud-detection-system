3efaec70a0f0dc0fb3ce5563439c81b8b07780f5
Rohan Grover <rohan7grover@gmail.com>
2023-10-16 16:03:58 +0530

    react basic setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes, Navigate
+} from "react-router-dom";
+
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+
+import Layout from './hocs/Layout';
+
+const App = () => {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="reset-password" element={<ResetPassword />} />
+          <Route exact path="/password/reset/confirm/:uid/:token" element={<ResetPasswordConfirm />} />
+          <Route exact path="/activate/:uid/:token" element={<Activate />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+};
+
+export default App;
