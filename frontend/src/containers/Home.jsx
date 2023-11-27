d1c859f658bf366ba9065ba57163a4644ccc4594
Rohan Grover <rohan7grover@gmail.com>
2023-11-27 13:34:09 +0530

    modified home to display user details

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { load_user } from '../actions/auth';
 import { Link } from 'react-router-dom';
 
-const Home = () => (
-  <div className='container'>
-    <div class='jumbotron mt-5'>
-      <h1 class='display-4'>Welcome to Auth System!</h1>
-      <p class='lead'>This is an incredible authentication system with production level features!</p>
-      <hr class='my-4' />
-      <p>Click the Log In button</p>
-      <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
-    </div>
-  </div>
-);
+const Home = ({ isAuthenticated, user, load_user }) => {
+  useEffect(() => {
+    load_user();
+  }, [load_user]);
 
-export default Home;
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <h2>Welcome, {user ? user.name : 'User'}!</h2>
+          <p>Email: {user ? user.email : ''}</p>
+          <p>Role: {user ? user.role : ''}</p>
+        </div>
+      ) : (
+        <div className='container'>
+          <div class='jumbotron mt-5'>
+            <h1 class='display-4'>Welcome to Auth System!</h1>
+            <p class='lead'>This is an incredible authentication system with production level features!</p>
+            <hr class='my-4' />
+            <p>Click the Log In button</p>
+            <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, { load_user })(Home);
