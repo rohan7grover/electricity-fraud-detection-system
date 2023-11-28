97e6ce231a90c173717cb1ed57f55946cc735a6f
Rohan Grover <rohan7grover@gmail.com>
2023-11-28 17:48:14 +0530

    moved css folder

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { login } from '../actions/auth';
+import '../css/Login.css';
 
 const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
